77ca1577e3d50aeb12b6c964ce60ec7cfe4ab13b
Your Name <you@example.com>
2024-09-02 23:58:38 -0400

    review and js

---
+++ script/amazon.js
@@ -0,0 +1,29 @@
+let data = [
+  {
+    image: "./images/products/black-2-slot-toaster.jpg",
+    name: "black and White",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceInCent: 1099,
+  },
+  {
+    image: "/images/products/intermediate-composite-basketball.jpg",
+    name: "intermediate size basketball",
+    rating: {
+      stars: 4.5,
+      count: 127,
+    },
+    priceInCent: 1099,
+  },
+  {
+    image: "/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton Tshirt-2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceInCent: 1099,
+  },
+];
