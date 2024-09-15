0003b383f6482a4bf3e10088a5121f4632d5a0bd
Your Name <you@example.com>
2024-09-15 11:31:38 -0400

    style checkout and generate an html using js

--- script/checkout.js
+++ script/checkout.js
@@ -1,5 +1,28 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+let checkoutGrid = document.querySelector(".checkout-grid");
 
-console.log("cart", cart);
-console.log("pr", products);
+let deliveryOptiontHTML = ``;
+
+// console.log("pr", products);
+
+// collect some data
+// generate an HTML with the collected data
+// make the HTML page interactive
+
+let matching;
+cart.forEach((cartItem) => {
+  let cartProductId;
+
+  cartProductId = cartItem.productId;
+
+  products.forEach((element) => {
+    if (cartProductId == element.id) {
+      matching = element;
+    }
+  });
+  console.log("image", matching.image);
+  deliveryOptiontHTML += ``;
+
+  //checkoutGrid.innerHTML = deliveryOptiontHTML;
+});
