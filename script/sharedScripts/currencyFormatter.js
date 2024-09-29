7b654ff1202a4b6544f5bbacf9f224ceacffa976
Your Name <you@example.com>
2024-09-29 10:55:16 -0400

    automatic test function to test currency formatter

--- script/sharedScripts/currencyFormatter.js
+++ script/sharedScripts/currencyFormatter.js
@@ -1,3 +1,3 @@
 export const currencyFormatter = (currency) => {
-  return (currency / 100).toFixed(2);
+  return (Math.round(currency) / 100).toFixed(2);
 };
---
+++ tests/currencyTest.js
@@ -0,0 +1,23 @@
+import { currencyFormatter } from "../script/sharedScripts/currencyFormatter.js";
+
+console.log("currencyFormater test suite");
+
+console.log("converts cents into dollars");
+currencyFormatter(2095) === "20.95"
+  ? console.log("passed")
+  : console.log("failed");
+
+console.log("works with 0");
+currencyFormatter(0) === "0.00" ? console.log("passed") : console.log("failed");
+
+console.log("round up to the nearest cent ");
+currencyFormatter(2000.5) === "20.01"
+  ? console.log("passed")
+  : console.log("failed");
+
+console.log("round down to the nearest cent");
+currencyFormatter(2000.4) === "20.00"
+  ? console.log("passed")
+  : console.log("failed");
+
+console.log("end of currencyFormatter test suite");
