ffedee54ca89d710e4dd736c90d8b76a5f055504
Your Name <you@example.com>
2024-10-03 11:29:02 -0400

    convert object to class

--- data/Cart-class.js
+++ data/Cart-class.js
@@ -1,112 +1,104 @@
-export let cart;
+class Cart {
+  cartItems = undefined;
+  localStorageKey = undefined;
 
-export function loadFromStorage() {
-  cart = JSON.parse(localStorage.getItem("cart"));
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
 
-  if (!cart) {
-    cart = [
-      {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: "1",
-      },
-      {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: "2",
-      },
-    ];
-  }
-}
-
-loadFromStorage();
-
-//function to save data to localstorage
-const saveToLocalStorage = () => {
-  localStorage.setItem("cart", JSON.stringify(cart));
-};
-
-//method to add new item if it doesn't exist or to
-// increment quantity by one if the item already exist
-//in the cart
-export const addToCart = (productId) => {
-  let matchingId;
-  cart.forEach((item) => {
-    if (item.productId == productId) {
-      matchingId = item;
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ];
     }
-  });
+  }
+  saveToLocalStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(cart));
+  }
 
-  if (matchingId) {
-    matchingId.quantity++;
-  } else {
-    cart.push({
-      productId: productId,
-      quantity: 1,
-      deliveryOptionId: "1",
+  addToCart(productId) {
+    let matchingId;
+    this.cartItems.forEach((item) => {
+      if (item.productId == productId) {
+        matchingId = item;
+      }
+    });
+
+    if (matchingId) {
+      matchingId.quantity++;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveToLocalStorage();
+  }
+
+  cartItemRemover(id) {
+    let newCart = [];
+    this.cartItems.forEach((item) => {
+      if (item.productId !== id) {
+        newCart.push(item);
+      }
+    });
+    this.cartItems = newCart;
+    this.saveToLocalStorage();
+  }
+  calculateItemQuantity() {
+    let totalItems = 0;
+    this.cartItems.forEach((item) => {
+      return (totalItems += item.quantity);
     });
   }
-  saveToLocalStorage();
-};
 
-//1 create a newcart array
-// 2 push all item whose id != paramid
-// 3 set cart to newcart
-export function cartItemRemover(id) {
-  let newCart = [];
-  cart.forEach((item) => {
-    if (item.productId !== id) {
-      newCart.push(item);
+  updateQuantity(id, newQuantity) {
+    let matching;
+    this.cartItems.forEach((item) => {
+      if (item.productId === id) {
+        matching = item;
+      }
+    });
+    if (matching) {
+      matching.quantity = newQuantity;
     }
-  });
-  cart = newCart;
-  saveToLocalStorage();
-}
-
-//calculate cart item quantities
-//and display it on the shopping cart
-let totalItems = 0;
-export function calculateItemQuantity() {
-  cart.forEach((item) => {
-    return (totalItems += item.quantity);
-
-    // shoppingCart.innerHTML = total;
-  });
-}
-
-//create a functon to update the quantity
-export const updateQuantity = (id, newQuantity) => {
-  let matching;
-  cart.forEach((item) => {
-    if (item.productId === id) {
-      matching = item;
-    }
-  });
-  if (matching) {
-    matching.quantity = newQuantity;
+    this.saveToLocalStorage();
   }
-  saveToLocalStorage();
-};
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matching;
+    this.cartItems.forEach((item) => {
+      if (item.productId === productId) matching = item;
+    });
 
-//function to find the macthing item in cart
-// and update its delivery option id
-// pass product id and delivery option id we got from html and
-//use them to find the maching item then update its delivery option id
-export const updateDeliveryOption = (productId, deliveryOptionId) => {
-  let matching;
-  cart.forEach((item) => {
-    if (item.productId === productId) matching = item;
-  });
+    matching.deliveryOptionId = deliveryOptionId;
+    this.saveToLocalStorage();
+  }
 
-  matching.deliveryOptionId = deliveryOptionId;
-  saveToLocalStorage();
-};
+  updateShoppingCart() {
+    let totalItems = 0;
+    this.cartItems.forEach((element) => {
+      totalItems += element.quantity;
+    });
 
-export const updateShoppingCart = () => {
-  let totalItems = 0;
-  cart.forEach((element) => {
-    totalItems += element.quantity;
-  });
+    return totalItems;
+  }
+}
 
-  return totalItems;
-};
+const cart = new Cart("cart-oop");
+const bcart = new Cart("besiness-cart");
+
+cart.loadFromStorage();
+cart.addToCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e");
+bcart.loadFromStorage();
+
+console.log("c", cart);
+console.log("bc", bcart);
--- script/checkout.js
+++ script/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary as orderSummary } from "./checkouts/orderSummary.js";
 import { renderPaymentSummary as paymentSummary } from "./checkouts/paymentSummary.js";
 import "../data/cart-oop.js";
+import "../data/Cart-class.js";
 orderSummary();
 paymentSummary();
