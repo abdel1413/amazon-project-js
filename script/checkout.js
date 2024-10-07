580813a00954488b5c147ce1deff2aad26172f4e
Your Name <you@example.com>
2024-10-07 12:27:02 -0400

    changed cart to cart class and export it all over

--- script/checkout.js
+++ script/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummary as orderSummary } from "./checkouts/orderSummary.js";
 import { renderPaymentSummary as paymentSummary } from "./checkouts/paymentSummary.js";
 import "../data/cart-oop.js";
-import "../data/Cart-class.js";
+import "../data/cart-class.js";
 import "../data/car.js";
+import { Cart } from "../data/cart-class.js";
+
 orderSummary();
 paymentSummary();
